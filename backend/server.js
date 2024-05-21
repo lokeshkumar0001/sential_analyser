48e0363c0ad95bfac772353a5c832b967c152f7e
lokeshkumar0001 <lokeshkrs2001@gmail.com>
2024-05-21 22:14:29 +0530

    add fetch

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors')
 
 const app = express();
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     const { spawn } = require('child_process');
